9080bc90cb7fc6eac00b7dbd61dc23609aa5211d
lippzhang <489435760@qq.com>
2023-09-25 17:24:12 +0800

    feat: 快照测试

--- tests/jest-setup.ts
+++ tests/jest-setup.ts
@@ -3,6 +3,20 @@ import mockConsole from "jest-mock-console";
 
 mockConsole()
 
+// 详情：https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
 // Object.defineProperty(global, 'localStorage', {
 //   value: {
 //     store: {} as Record<string, string>,
